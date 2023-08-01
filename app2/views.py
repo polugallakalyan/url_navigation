8e8fb74ac20ca6ada19802541d7e963b5ea4edc2
Polugallakalyan <Kalyanpolugalla@gmail.com>
2023-08-01 12:57:52 -0800

    first commit

---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def polu(request):
+    return render(request,'polu.html')
---
+++ app3/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def Thanvi(request):
+    return render(request,'Thanvi.html')
